b68fb220d5041c78386e2baa15323bb353c8483d
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-08-02 22:13:57 +0900

    Update Seebeck_analysis.f90

--- Seebeck_analysis.f90
+++ Seebeck_analysis.f90
@@ -1833,9 +1833,10 @@ PROGRAM seebeck_analysis
   WRITE(20,'(A)') hdr
   !
   OPEN(UNIT=21, FILE='ABGV2D.dat')
-  WRITE(21,*) "! A(E,T), GV^2*DOS: [(m/s)^2 * states/eV/unitcell]"
+  WRITE(21,*) "! A(E,T): [(m/s)^2 * states/eV/unitcell]"
   WRITE(21,*) "! B(E,T): [(m/s)^2 * states/eV^2/unitcell]"
-  WRITE(21,*) "# T [K]    E [eV]       E-mu [eV]    A(E,T)       B(E,T)       GV^2*DOS"
+  WRITE(21,*) "! <GV^2*DOS>: [(m/s)^2 * states/eV^2/unitcell]"
+  WRITE(21,*) "# T [K]    E [eV]       <E-mu> [eV]  A(E,T)       B(E,T)       <GV^2*DOS>"
 
   ! ------------------------------------------------------------------
   ! Step 6: Loop over temperatures to compute Seebeck coefficient
