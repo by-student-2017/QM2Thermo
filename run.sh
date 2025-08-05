c5d1881b1bf5d23baf65f0218233bada932395c8
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-08-05 20:29:18 +0900

    Add files via upload

--- run.sh
+++ run.sh
@@ -54,9 +54,9 @@ else
     gnuplot plot_ABGV2D.gpl
 fi
 
-echo "----------------------------------------------------------------------------"
-echo "The above 'warning' is for convenience on Windows, so don't worry about it."
+echo "----------------------------------------------------------------------------------"
+echo '## set terminal win font "Arial,12"                                             ##'
+echo "## warning: unknown or ambiguous terminal type; type 'set terminal' for a list. ##"
 echo ""
-echo 'set terminal win font "Arial,12"'
-echo "warning: unknown or ambiguous terminal type; type 'set terminal' for a list."
-echo "----------------------------------------------------------------------------"
+echo "The above 'warning' is for convenience on Windows, so don't worry about it."
+echo "----------------------------------------------------------------------------------"
