701ce683b97e2a3c7f504e6b7c774e526fb968a9
By STUDENT <33742249+by-student-2017@users.noreply.github.com>
2025-08-08 17:15:11 +0900

    Add files via upload

--- Shear_modulus_calculation/Abinit/compute_shear_modulus_from_stress_ab.awk
+++ Shear_modulus_calculation/Abinit/compute_shear_modulus_from_stress_ab.awk
@@ -8,7 +8,7 @@ BEGIN {
     # 1 [Ry/Bohr^3] = 2.179872361e-18 / 1.481847e-31 = 1.47105078e13 [Pa] = 1.47105078e4 [GPa]
     #conversion_factor = 14710.5  # Ry/Bohr^3 -> GPa
     conversion_factor = 1.0      # GPa
-    
+
     epsilon0 =  ""
     epsilon1 = -0.010
     epsilon2 = -0.005
@@ -18,17 +18,33 @@ BEGIN {
 }
 {
     print "Read:", $1, $5
-    strain[$1 + 0] = $5
+    stress[$1 + 0] = $5
+    Ly[NR] = $10 + 0
 }
 END {
-    # Central difference approximation: G = (sigma(+e) - sigma(-e)) / (2 * e)
-    if ((strain[epsilon5] != "") && (strain[epsilon1] != "")) {
-        G1 = (strain[epsilon5] - strain[epsilon1]) / (2 * epsilon5)
-        printf("Shear modulus (G) from +/- %.3f strain: %.6f GPa\n", epsilon5, G1*conversion_factor)
-    }
-    if ((strain[epsilon4] != "") && (strain[epsilon2] != "")) {
-        G2 = (strain[epsilon4] - strain[epsilon2]) / (2 * epsilon4)
-        printf("Shear modulus (G) from +/- %.3f strain: %.6f GPa\n", epsilon4, G2*conversion_factor)
-    }
-    printf("Average Shear modulus (G): %.6f GPa\n", (G1+G2)/2*conversion_factor)
+
+    printf("Ly = %.6f [Angstrom] \n", Ly[4])
+
+    gamma = (epsilon1 - epsilon3)/Ly[4]
+    G1 = -(stress[epsilon1] - stress[epsilon3]) / gamma
+
+    printf("Shear modulus G from energy fit (epislon = %.6f): %.6f Ry/Bohr^3 = %.2f GPa\n", epsilon1, G1, G1 * conversion_factor)
+
+    gamma = (epsilon2 - epsilon3)/Ly[4]
+    G2 = -(stress[epsilon2] - stress[epsilon3]) / gamma
+
+    printf("Shear modulus G from energy fit (epislon = %.6f): %.6f Ry/Bohr^3 = %.2f GPa\n", epsilon2, G2, G2 * conversion_factor)
+
+    gamma = (epsilon4 - epsilon3)/Ly[4]
+    G4 = -(stress[epsilon4] - stress[epsilon3]) / gamma
+
+    printf("Shear modulus G from energy fit (epislon = %.6f): %.6f Ry/Bohr^3 = %.2f GPa\n", epsilon4, G4, G4 * conversion_factor)
+
+    gamma = (epsilon5 - epsilon3)/Ly[4]
+    G5 = -(stress[epsilon5] - stress[epsilon3]) / gamma
+    
+    printf("Shear modulus G from energy fit (epislon = %.6f): %.6f Ry/Bohr^3 = %.2f GPa\n", epsilon5, G5, G5 * conversion_factor)
+    
+    G = (G1 + G2 + G4 + G5)/4
+    printf("Average Shear modulus G from energy fit: %.6f Ry/Bohr^3 = %.2f GPa\n", G, G * conversion_factor)
 }
--- Shear_modulus_calculation/Abinit/run_shear_ab.sh
+++ Shear_modulus_calculation/Abinit/run_shear_ab.sh
@@ -10,7 +10,7 @@ base_input="case.scf.in"
 # Output file for stress results
 results_file="shear_results.txt"
 > "$results_file"
-echo "#strain     energy[eV]      volume[bohr^3]    s_xx[GPa]       s_xy[GPa]       s_xz[GPa]       s_yy[GPa]       s_yz[GPa]       s_zz[GPa]" > "$results_file"
+echo "#strain     energy[eV]      volume[bohr^3]    s_xx[GPa]       s_xy[GPa]       s_xz[GPa]       s_yy[GPa]       s_yz[GPa]       s_zz[GPa] Ly[Angstrom]" > "$results_file"
 
 # Strain values to apply
 strain_values=(-0.010 -0.005 +0.000 +0.005 +0.010)
@@ -22,22 +22,53 @@ mkdir -p log
 for strain in "${strain_values[@]}"; do
     input_file="log/case.scf.${strain}.in"
     output_file="log/case.scf.${strain}.out"
-
+    
+    # Get A in &SYSTEM section
+    A=$(awk '/acell / {print $2; exit}' "$base_input")
+    B=$(awk '/acell / {print $3; exit}' "$base_input")
+    C=$(awk '/acell / {print $4; exit}' "$base_input")
+    echo "lattice parameter A: ${A}"
+    echo "lattice parameter B: ${B}"
+    echo "lattice parameter C: ${C}"
+    
     # Generate strained input file using awk
-    awk -v strain="${strain}" '
+    awk -v strain="${strain}" -v A="$A" -v B="$B" -v C="$C" '
     BEGIN {in_cell=0; line=0}
     /rprim/ {in_cell=1}
     in_cell {
         line++
-        if (line==1) {
-            $3 = sprintf("%19.15f", $3 + strain)
+        #---------------------------------------
+        # Distortion is introduced in this range.
+        if (line==2) {
+            $1 = sprintf("%19.15f", $1 + (strain/B))
         }
+        #---------------------------------------
         print
         if (line==3) in_cell=0
         next
     }
     {print}
     ' "$base_input" > "$input_file"
+
+    # Get Ly
+    Ly=$(awk -v A="$A" -v B="$B" -v C="$C" '
+    BEGIN {in_cell=0; line=0}
+    /rprim/ {in_cell=1}
+    in_cell {
+        line++
+        #---------------------------------------
+        # Distortion is introduced in this range.
+        if (line==2) { # a2 line
+          len = sqrt(($1*A)*($1*A) + ($2*B)*($2*B) + ($3*C)*($3*C))
+          printf("%19.15f", len)
+          exit
+        }
+        #---------------------------------------
+        if (line==3) in_cell=0
+        next
+    }
+    ' "$base_input")
+    echo "lattice parameter Ly:", ${Ly}
 
     # Run Abinit and extract stress tensor
     mpirun -np ${NCPUs} abinit "$input_file" | tee "$output_file"
@@ -60,8 +91,8 @@ for strain in "${strain_values[@]}"; do
         }' "$output_file")
 
     # Output strain, energy, volume, and stress tensor components
-    printf "%+8.4f %15.8f %15.8f %15.8f %15.8f %15.8f %15.8f %15.8f %15.8f\\n" \
-    "$strain" "$energy" "$volume" "$xx" "$xy" "$xz" "$yy" "$yz" "$zz" >> "$results_file"
+    printf "%+8.4f %15.8f %15.8f %15.8f %15.8f %15.8f %15.8f %15.8f %15.8f %15.8f\\n" \
+    "$strain" "$energy" "$volume" "$xx" "$xy" "$xz" "$yy" "$yz" "$zz" "$Ly">> "$results_file"
 
 done
 
@@ -70,11 +101,6 @@ echo ""
 # Evaluate shear modulus using AWK
 echo "command: awk -f compute_shear_modulus_from_stress_ab.awk shear_results.txt"
 awk -f compute_shear_modulus_from_stress_ab.awk shear_results.txt
-echo ""
-echo "The method using energy does not match the method using"
-echo "stress unless the calculation conditions are made more precise."
-echo "command: awk -f compute_shear_modulus_from_energy_ab.awk shear_results.txt"
-awk -f compute_shear_modulus_from_energy_ab.awk shear_results.txt
 
 rm -f caseo_DDB caseo_DEN caseo_EBANDS.agr caseo_WFK
 rm -f caseo_EIG caseo_EIG.nc caseo_GSR.nc caseo_OUT.nc 
